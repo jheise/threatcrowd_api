3a9653c70b203feeee1f77d3f94405dfb3988f38
Jon Heise <j.heise@gmail.com>
2016-02-15 23:20:31 -0800

    adding class to handle memoizing calls

--- threat_test.py
+++ threat_test.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from threatcrowd import *
+import time
 
 print email_report("name@email.com")
 print ip_report("4.2.2.1")
 print domain_report("google.com")
 print antivirus_report("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*")
+
+tc = ThreatCrowd(ttl=5)
+print tc.ip_report("4.2.2.1")
+print tc.ip_report("4.2.2.1")
+
+print "sleeping longer than the ttl"
+time.sleep(6)
+print tc.ip_report("4.2.2.1")
--- threatcrowd.py
+++ threatcrowd.py
@@ -1,19 +1,67 @@
 import requests
 import json
+import time
 
-def __threatcrowd_query(path, params):
+def _threatcrowd_query(path, params):
     full_path = "http://www.threatcrowd.org/searchApi/v2/{0}/report/".format(path)
     data = requests.get(full_path, params=params).text
     return json.loads(data)
 
 def email_report(address):
-    return __threatcrowd_query("email", {"email":address})
+    return _threatcrowd_query("email", {"email":address})
 
 def ip_report(address):
-    return __threatcrowd_query("ip", {"ip":address})
+    return _threatcrowd_query("ip", {"ip":address})
 
 def domain_report(domain):
-    return __threatcrowd_query("domain", {"domain":domain})
+    return _threatcrowd_query("domain", {"domain":domain})
 
 def antivirus_report(antivirus):
-    return __threatcrowd_query("antivirus", {"antivirus":antivirus})
+    return _threatcrowd_query("antivirus", {"antivirus":antivirus})
+
+class ThreatCrowd(object):
+    """
+    Class to handle memozing calls
+    """
+
+    def __init__(self, ttl=14400):
+        super(ThreatCrowd, self).__init__()
+        self.ttl = ttl
+        self.calls = {
+            "email":{},
+            "ip":{},
+            "domain":{},
+            "antivirus":{}
+        }
+
+    def email_report(self, address):
+        return self.__process_call("email", address)
+
+    def ip_report(self, address):
+        return self.__process_call("ip", address)
+
+    def domain_report(self, domain):
+        return self.__process_call("domain", domain)
+
+    def antivirus_report(self, antivirus):
+        return self.__process_call("antivirus", antivirus)
+
+    def __process_call(self, path, param):
+        current_time = time.time()
+
+        if param in self.calls[path]:
+            diff = current_time - self.calls[path][param]["time"]
+            if diff < self.ttl:
+                print "returning value form memory"
+                return self.calls[path][param]["result"]
+
+        # either there isnt a result or the ttl has expired, query from source
+        print "query for new result"
+        result = _threatcrowd_query(path, {path:param})
+        if param not in self.calls[path]:
+            self.calls[path][param] = {}
+
+        self.calls[path][param]["result"] = result
+        self.calls[path][param]["time"] = current_time
+
+        return result
