56352723c8b5b4301edd960fa306e79eb8809d43
Jon Heise <j.heise@gmail.com>
2016-08-29 23:18:34 -0700

    switching from distutils to setuptools, adding requests[security] as a requirement

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup( name="threatcrowd",
-       version='0.4',
+       version='0.5',
        py_modules=['threatcrowd'],
-       requires=['requests(>=2.4.0)'],
-       install_requires=['requests>=2.4.0'],
+       install_requires=['requests>=2.4.0','requests>=2.4.0'],
        url="https://github.com/jheise/threatcrowd_api",
        author="Jon Heise",
        author_email="j.heise@gmail.com",
